4e3e01465e4062ee4c787a0054c33f19dc94bd77
Yuriy Klepka <yuriyklepka@gmail.com>
2018-04-17 10:45:36 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Index</title>
+    <link href="css/main.css" rel="stylesheet">
+</head>
+<body>
+<div class="header">
+    <ul class="menu">
+        <li class="green"><a href="#" title="">Меню 1</a>
+        <li><a class="blue" href="#" title="">Меню 2</a>
+        <li><a href="#" title="">Меню 3</a>
+        <li><a class="blue"  href="#" title="">Меню 4</a>
+    </ul>
+    <form action="" method="post">
+        <input class="text" type="text" name="NAME" value=""/>
+        <input class="text" type="text" name="EMAIL" value=""/>
+        <input class="text" type="text" name="PHONE" value=""/>
+        <input type="submit" name="save" value="Сохранить"/>
+    </form>
+</div>
+<script src="js/main.js"></script>
+</body>
+</html>
